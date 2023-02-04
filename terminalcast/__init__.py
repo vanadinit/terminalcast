f9ec61ab7680072f02e4650619ac3934b509e40c
Johannes Paul <vanadinit@quantentunnel.de>
2023-02-05 00:38:01 +0100

    Just some formatting

--- terminalcast/__init__.py
+++ terminalcast/__init__.py
@@ -9,12 +9,13 @@ VERSION = metadata.version('terminalcast')
 
 def main():
     print(f'Terminalcast Version {VERSION}')
-    print('-----')
+
 
     parser = ArgumentParser(prog='terminalcast', description='Cast local videos to your chromecast')
     parser.add_argument('filepath', help='file path')
     args = parser.parse_args()
 
+    print('----- File information -----')
     media_file_data = FileMetadata(filepath=args.filepath)
     print(media_file_data.details())
     # TODO audio stream selection
@@ -22,12 +23,13 @@ def main():
     #  * provide selection possibiliy
     #  * reencode with ffmpeg if not the first stream, delete all others -> temp file
 
-    print('-----')
-
+    print('----- Initializing Chromecast -----')
     tc = TerminalCast(filepath=args.filepath)
     print(f'Chromecast: {tc.cast.cast_info.friendly_name}')
     print(f'Status: {tc.cast.status}')
-    print('-----')
+
+    print('----- Starting HTTP Server -----')
     tc.start_server()
-    print('-----')
+
+    print('----- Start playing video -----\n')
     tc.play_video()
