6c5e1c237081cebb1e2062700b75d0392b1b608b
Johannes Paul <vanadinit@quantentunnel.de>
2023-09-25 00:52:13 +0200

    Add audio selection support

--- terminalcast/__init__.py
+++ terminalcast/__init__.py
@@ -1,7 +1,12 @@
+import os
 from argparse import ArgumentParser
 from importlib import metadata
+from tempfile import mkstemp
+
+import ffmpeg
 
 from .filedata import FileMetadata
+from .helper import selector
 from .tc import TerminalCast
 
 VERSION = metadata.version('terminalcast')
@@ -10,7 +15,6 @@ VERSION = metadata.version('terminalcast')
 def main():
     print(f'Terminalcast Version {VERSION}')
 
-
     parser = ArgumentParser(prog='terminalcast', description='Cast local videos to your chromecast')
     parser.add_argument('filepath', help='file path')
     args = parser.parse_args()
@@ -18,13 +22,32 @@ def main():
     print('----- File information -----')
     media_file_data = FileMetadata(filepath=args.filepath)
     print(media_file_data.details())
-    # TODO audio stream selection
-    #  * check with ffmpeg, it there are multiple streams
-    #  * provide selection possibiliy
-    #  * reencode with ffmpeg if not the first stream, delete all others -> temp file
+
+    audio_stream = selector(entries=[
+        (stream, f'Audio: {stream.title}')
+        for stream in media_file_data.audio_streams
+    ])
+
+    tmp_file_path = ''
+    if audio_stream and audio_stream != media_file_data.audio_streams[0]:
+        tmp_file_path = mkstemp(
+            suffix='.mp4',
+            prefix=f'terminalcast_pid{os.getpid()}_',
+            dir='/var/tmp' if os.path.isdir('/var/tmp') else None)[1]
+        os.remove(tmp_file_path)
+
+        print(f'Create temporary video file at {tmp_file_path}')
+        input = ffmpeg.input(args.filepath, loglevel='error')
+        video = input['v']
+        audio = input[audio_stream.index[-1:]]
+        ffmpeg.output(
+            video, audio, tmp_file_path,
+            codec='copy',
+        ).run()
+        print(f'Video created')
 
     print('----- Initializing Chromecast -----')
-    tc = TerminalCast(filepath=args.filepath)
+    tc = TerminalCast(filepath=tmp_file_path or args.filepath)
     print(f'Chromecast: {tc.cast.cast_info.friendly_name}')
     print(f'Status: {tc.cast.status}')
 
